be8354f20998db3eec9c5b318b0f8241d9f83dfa
samuelponraj14 <samuelponraj14@gmail.com>
2024-11-10 11:03:49 +0530

    name

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export const metadata = {
 	  url: "https://medicmode-updated.vercel.app/", 
 	  images: [
 		{
-		  url: "/assets/medicmode.png", 
+		  url: "https://drive.google.com/file/d/1jyz9gBWj_W9JUsQ2B1QskPTaMJ6o6N1n/view?usp=drive_link", 
 		  width: 1200,
 		  height: 630,
 		  alt: "Medic Mode - Emergency Medical Gazette"
