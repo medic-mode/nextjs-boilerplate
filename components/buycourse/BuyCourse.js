7f876fd33ab0386bc51cd9c47508c74fc491cec0
samuel-ponraj <samuelponraj.official@gmail.com>
2024-12-09 20:24:56 +0530

    name

--- components/buycourse/BuyCourse.js
+++ components/buycourse/BuyCourse.js
@@ -59,7 +59,7 @@ const BuyCourse = ({ id, price, title }) => {
       });
       return;
     }
-
+  
     try {
       // Call the API to create an order
       const response = await fetch('/api/createOrder', {
@@ -78,34 +78,105 @@ const BuyCourse = ({ id, price, title }) => {
   
       const { id: orderId } = data.order;
   
-    // Initialize purchaseDetails
-    const purchaseDetails = {
-      courseId: id,
-      courseTitle: title,
-      purchaseDate: new Date().toISOString(),
-      paymentId: null, // Will be updated upon payment success
-      paymentStatus: null, // Will be updated to 'Success' or 'Failed'
-      paymentAmount: price,
-      failureReason: null, // Will be updated if the payment fails
-    };
+      // Initialize purchaseDetails
+      const purchaseDetails = {
+        courseId: id,
+        courseTitle: title,
+        purchaseDate: new Date().toISOString(),
+        paymentId: null, // Will be updated upon payment success
+        paymentStatus: null, // Will be updated to 'Success' or 'Failed'
+        paymentAmount: price,
+        failureReason: null, // Will be updated if the payment fails
+      };
   
-    const options = {
-      key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
-      amount: price * 100,
-      currency: 'INR',
-      name: 'Medicmode',
-      description: `Payment for ${title}`,
-      order_id: orderId,
-      handler: async (response) => {
-        // Update payment details on success
-        purchaseDetails.paymentId = response.razorpay_payment_id;
-        purchaseDetails.paymentStatus = 'Success';
+      const options = {
+        key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+        amount: price * 100,
+        currency: 'INR',
+        name: 'Medicmode',
+        description: `Payment for ${title}`,
+        order_id: orderId,
+        handler: async (response) => {
+          // Update payment details on success
+          purchaseDetails.paymentId = response.razorpay_payment_id;
+          purchaseDetails.paymentStatus = 'Success';
   
-        alert(`Payment Successful! Razorpay Payment ID: ${response.razorpay_payment_id}. Our Team will contact you soon.`);
-        toast.success('Payment Successful!', { duration: 3000 });
+          alert(
+            `Payment Successful! Razorpay Payment ID: ${response.razorpay_payment_id}. Our Team will contact you soon.`
+          );
+          toast.success('Payment Successful!', { duration: 3000 });
+  
+          try {
+            const userDocRef = query(
+              collection(db, 'users'),
+              where('email', '==', userEmail)
+            );
+            const querySnapshot = await getDocs(userDocRef);
+  
+            if (!querySnapshot.empty) {
+              const userDoc = querySnapshot.docs[0];
+              const userDocRef = userDoc.ref;
+  
+              // Update the user's purchase history
+              await updateDoc(userDocRef, {
+                purchaseHistory: arrayUnion(purchaseDetails),
+              });
+  
+              // Send purchase details to the API
+              await fetch('/api/sendPurchaseDetails', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ userDetails, purchaseDetails }),
+              });
+            } else {
+              toast.alert('User not found. Please try again.', {
+                duration: 3000,
+              });
+            }
+          } catch (error) {
+            toast.alert(
+              'Failed to save purchase details. Please contact support.',
+              { duration: 3000 }
+            );
+          }
+        },
+        prefill: {
+          name: userDetails.firstName,
+          email: userDetails.email || userEmail,
+          contact: userDetails.phone
+            ? userDetails.phone.toString().slice(-10)
+            : '',
+        },
+        notes: {
+          course_id: id,
+        },
+        theme: {
+          color: '#3399cc',
+        },
+      };
+  
+      const rzp = new window.Razorpay(options);
+      rzp.open();
+  
+      rzp.on('payment.failed', async function (response) {
+        // Update purchaseDetails for failure
+        purchaseDetails.paymentId =
+          response.error.metadata.payment_id || 'N/A';
+        purchaseDetails.paymentStatus = 'Failed';
+        purchaseDetails.failureReason = response.error.description;
+  
+        alert('Payment failed. Please try again.');
+        toast.alert('Payment Failed: ' + response.error.description, {
+          duration: 3000,
+        });
   
         try {
-          const userDocRef = query(collection(db, 'users'), where('email', '==', userEmail));
+          const userDocRef = query(
+            collection(db, 'users'),
+            where('email', '==', userEmail)
+          );
           const querySnapshot = await getDocs(userDocRef);
   
           if (!querySnapshot.empty) {
@@ -116,68 +187,24 @@ const BuyCourse = ({ id, price, title }) => {
             await updateDoc(userDocRef, {
               purchaseHistory: arrayUnion(purchaseDetails),
             });
-  
-            // Send purchase details to the API
-            await fetch('/api/sendPurchaseDetails', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({ userDetails, purchaseDetails }),
-            });
           } else {
-            toast.alert('User not found. Please try again.', { duration: 3000 });
+            toast.alert('User not found. Please try again.', {
+              duration: 3000,
+            });
           }
         } catch (error) {
-          toast.alert('Failed to save purchase details. Please contact support.', { duration: 3000 });
+          toast.alert(
+            'Failed to save failed payment details. Please contact support.',
+            { duration: 3000 }
+          );
         }
-      },
-      prefill: {
-        name: userDetails.firstName,
-        email: userDetails.email || userEmail,
-        contact: userDetails.phone ? userDetails.phone.toString().slice(-10) : '',
-      },
-      notes: {
-        course_id: id,
-      },
-      theme: {
-        color: '#3399cc',
-      },
-    };
-  
-    const rzp = new window.Razorpay(options);
-    rzp.open();
-  
-    rzp.on('payment.failed', async function (response) {
-      // Update purchaseDetails for failure
-      purchaseDetails.paymentId = response.error.metadata.payment_id || 'N/A';
-      purchaseDetails.paymentStatus = 'Failed';
-      purchaseDetails.failureReason = response.error.description;
-  
-      alert('Payment failed. Please try again.');
-      toast.alert('Payment Failed: ' + response.error.description, { duration: 3000 });
-  
-      try {
-        const userDocRef = query(collection(db, 'users'), where('email', '==', userEmail));
-        const querySnapshot = await getDocs(userDocRef);
-  
-        if (!querySnapshot.empty) {
-          const userDoc = querySnapshot.docs[0];
-          const userDocRef = userDoc.ref;
-  
-          // Update the user's purchase history
-          await updateDoc(userDocRef, {
-            purchaseHistory: arrayUnion(purchaseDetails),
-          });
-        } else {
-          toast.alert('User not found. Please try again.', { duration: 3000 });
-        }
-      } catch (error) {
-        toast.alert('Failed to save failed payment details. Please contact support.', { duration: 3000 });
-      }
-    });
+      });
+    } catch (error) {
+      toast.alert('Failed to initiate payment. Please try again.', {
+        duration: 3000,
+      });
+    }
   };
-  
 
   return (
     <div>
