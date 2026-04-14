5b27e2098d5d613ab3790a7cfcdc9faaddb5c254
samuel-ponraj <samuelponraj.official@gmail.com>
2026-04-14 20:58:36 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
+  experimental: {
+    turbopack: {}, 
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
